44dfe9e3bc6a03f51e34248c3da21b5e51ad53ca
Seysa <garion.g@hotmail.fr>
2022-06-14 00:52:33 +0200

    added smooth meteo transition

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div
-    class="
-      bg-black
-      text-white
-      overflow-hidden
-      font-['Roboto']
-      w-screen
-      h-screen
-      flex flex-col
-      gap-4
-      justify-center
-      items-center
-    "
+    class="bg-black text-white overflow-hidden font-['Roboto'] w-screen h-screen flex flex-col gap-4 justify-center items-center"
   >
     <button
       @click="hidden = !hidden"
-      class="
-        px-4
-        py-2
-        text-black
-        hover:text-white
-        border-2 border-black
-        hover:border-white
-        rounded
-        transition-all
-        duration-500
-      "
+      class="px-4 py-2 text-black hover:text-white border-2 border-black hover:border-white rounded transition-all duration-500"
     >
       {{ buttonText }}
     </button>
     <h1 id="time" class="text-7xl">{{ time }}</h1>
-    <h2 id="meteo" v-show="!hidden" class="flex flex-col text-gray-300">
+    <h2
+      id="meteo"
+      v-show="!hidden"
+      class="flex flex-col text-black transition-all duration-1000"
+      :class="{
+        'text-gray-300': hasData,
+      }"
+    >
       <span id="temp">{{ temp }}°C</span>
       <span id="humidity">{{ humidity }}% humidity</span>
       <div>
@@ -45,10 +31,10 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { queryMeteo } from "./meteo";
-
-const hidden = ref(false);
 const buttonText = computed(() => (!hidden.value ? "Hide" : "Show"));
 
+const hidden = ref(false);
+const hasData = ref(false);
 const time = ref("hh:mm");
 const temp = ref(-99);
 const humidity = ref(-99);
@@ -72,6 +58,7 @@ async function updateWeather() {
   humidity.value = data.current.humidity;
   sunrise.value = data.current.sunrise * 1000;
   sunset.value = data.current.sunset * 1000;
+  hasData.value = true;
 }
 
 function updateTime() {
