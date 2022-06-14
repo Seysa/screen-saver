8189834ad150001c55e6a5d9cbe352a5b3a2e87d
Seysa <garion.g@hotmail.fr>
2022-06-14 03:36:09 +0200

    used meteo component

--- src/App.vue
+++ src/App.vue
@@ -9,54 +9,15 @@
       {{ buttonText }}
     </button>
     <TheTimer />
-    <h2
-      id="meteo"
-      v-show="!hidden"
-      class="flex flex-col text-black transition-all duration-1000"
-      :class="{
-        'text-gray-300': hasData,
-      }"
-    >
-      <span id="temp">{{ temp }}°C</span>
-      <span id="humidity">{{ humidity }}% humidity</span>
-      <div>
-        <span id="sunrise">{{ sunriseHour }}</span>
-        <span class="mx-2">→</span>
-        <span id="sunset">{{ sunsetHour }}</span>
-      </div>
-    </h2>
+    <TheMeteo v-show="!hidden" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { queryMeteo } from "./meteo";
 import TheTimer from "./components/TheTimer.vue";
-import { toHour } from "./time";
-
-const buttonText = computed(() => (!hidden.value ? "Hide" : "Show"));
+import TheMeteo from "./components/TheMeteo.vue";
 
 const hidden = ref(false);
-const hasData = ref(false);
-const temp = ref(-99);
-const humidity = ref(-99);
-const sunrise = ref(-99);
-const sunset = ref(-99);
-const sunriseHour = computed(() => toHour(sunrise.value));
-const sunsetHour = computed(() => toHour(sunset.value));
-
-async function updateWeather() {
-  console.info("query meteo...");
-  const data = await queryMeteo();
-  console.log(data);
-  const roundedTemp = Math.round(data.current.temp);
-  temp.value = roundedTemp;
-  humidity.value = data.current.humidity;
-  sunrise.value = data.current.sunrise * 1000;
-  sunset.value = data.current.sunset * 1000;
-  hasData.value = true;
-}
-
-updateWeather();
-setInterval(updateWeather, 1000 * 60 * 2); // 2 minutes
+const buttonText = computed(() => (!hidden.value ? "Hide" : "Show"));
 </script>
---
+++ src/components/TheMeteo.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2
+    id="meteo"
+    class="flex flex-col text-black transition-all duration-1000"
+    :class="{
+      'text-gray-300': hasData,
+    }"
+  >
+    <span id="temp">{{ temp }}°C</span>
+    <span id="humidity">{{ humidity }}% humidity</span>
+    <div>
+      <span id="sunrise">{{ sunriseHour }}</span>
+      <span class="mx-2">→</span>
+      <span id="sunset">{{ sunsetHour }}</span>
+    </div>
+  </h2>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { queryMeteo } from "../meteo";
+import { toHour } from "../time";
+
+const hasData = ref(false);
+const temp = ref(-99);
+const humidity = ref(-99);
+const sunrise = ref(-99);
+const sunset = ref(-99);
+const sunriseHour = computed(() => toHour(sunrise.value));
+const sunsetHour = computed(() => toHour(sunset.value));
+
+async function updateWeather() {
+  console.info("query meteo...");
+  const data = await queryMeteo();
+  console.log(data);
+  const roundedTemp = Math.round(data.current.temp);
+  temp.value = roundedTemp;
+  humidity.value = data.current.humidity;
+  sunrise.value = data.current.sunrise * 1000;
+  sunset.value = data.current.sunset * 1000;
+  hasData.value = true;
+}
+
+updateWeather();
+setInterval(updateWeather, 1000 * 60 * 2); // 2 minut
+</script>
